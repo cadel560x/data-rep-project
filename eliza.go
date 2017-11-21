af00f1bf4a583a86864bf6d1f0fd859a5ff4f10e
JAVIER MANTILLA - Student <G00329649@gmit.ie>
2017-11-21 21:37:11 +0000

    style changed

--- eliza.go
+++ eliza.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 )
@@ -9,9 +11,21 @@ func redirect(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "http://localhost:8080/eliza", 301)
 }
 
-func main() {
+func handlerEliza(w http.ResponseWriter, r *http.Request) {
+	// http.ServeFile(w, r, "eliza.html")
 
+	templ, _ := template.ParseFiles("eliza.html")
+
+	templ.Execute(w, interface{})
+	// templ.Execute(w, placeholders)
+}
+
+func main() {
 	http.HandleFunc("/", redirect)
+	http.HandleFunc("/eliza", handlerEliza)
+
+	fmt.Println("Server running at port 8080...")
+
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
