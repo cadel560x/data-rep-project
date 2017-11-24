7127f918b56ee4a53dff7a6f5d2ffc60650fb8ab
JAVIER MANTILLA - Student <G00329649@gmit.ie>
2017-11-24 18:01:20 +0000

    background image add

--- main.go
+++ main.go
@@ -1,24 +1,9 @@
 package main
 
 import (
-	// "log"
-	// "net/http"
-
 	"./eliza"
 )
 
 func main() {
-	// Creating a reference to 'eliza.AjaxHandler'
-	// var ajaxHandler = eliza.AjaxHandler
-
-	// http.HandleFunc("/ajax", ajaxHandler)
-	// http.Handle("/", http.FileServer(http.Dir("./html")))
-
-	// log.Printf("Starting server at port 8080...")
-	// err := http.ListenAndServe(":8080", nil)
-	// if err != nil {
-	// 	log.Fatal("ListenAndServe: ", err)
-	// }
-
 	eliza.Start();
 }
